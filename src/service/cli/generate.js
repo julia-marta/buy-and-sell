41398cb7936eeb2bd17962afe7b8f28dc9d28b5f
julia-marta <marta237@yandex.ru>
2021-02-04 01:52:04 +0300

    Программирует консольное приложение для обработки команд (помощь, версия и генерация моковых объявлений), запускает и тестирует работу команд, генерирует моки

---
+++ src/service/cli/generate.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const fs = require(`fs`);
+
+const {
+  getRandomInt,
+  shuffleArray,
+} = require(`../../utils`);
+
+const DEFAULT_COUNT = 1;
+const FILE_NAME = `mocks.json`;
+const MAX_DESCRIPTION_LENGTH = 5;
+
+const TITLES = [
+  `Продам книги Стивена Кинга`,
+  `Продам новую приставку Sony Playstation 5`,
+  `Продам отличную подборку фильмов на VHS`,
+  `Куплю антиквариат`,
+  `Куплю породистого кота`,
+  `Продам коллекцию журналов «Огонёк»`,
+  `Отдам в хорошие руки подшивку «Мурзилка»`,
+  `Продам советскую посуду. Почти не разбита`,
+  `Куплю детские санки`,
+];
+
+const DESCRIPTIONS = [
+  `Товар в отличном состоянии.`,
+  `Пользовались бережно и только по большим праздникам.`,
+  `Продаю с болью в сердце...`,
+  `Бонусом отдам все аксессуары.`,
+  `Даю недельную гарантию.`,
+  `Если товар не понравится — верну всё до последней копейки.`,
+  `Это настоящая находка для коллекционера!`,
+  `Если найдёте дешевле — сброшу цену.`,
+  `Таких предложений больше нет!`,
+  `Две страницы заляпаны свежим кофе.`,
+  `При покупке с меня бесплатная доставка в черте города.`,
+  `Кажется, что это хрупкая вещь.`,
+  `Мой дед не мог её сломать.`,
+  `Кому нужен этот новый телефон, если тут такое...`,
+  `Не пытайтесь торговаться. Цену вещам я знаю.`,
+];
+
+const CATEGORIES = [
+  `Книги`,
+  `Разное`,
+  `Посуда`,
+  `Игры`,
+  `Животные`,
+  `Журналы`,
+];
+
+const OfferType = {
+  OFFER: `offer`,
+  SALE: `sale`,
+};
+
+const SumRestrict = {
+  MIN: 1000,
+  MAX: 100000,
+};
+
+const PictureRestrict = {
+  MIN: 1,
+  MAX: 16,
+};
+
+const getPictureFileName = (number) => number > 10 ? `item${number}.jpg` : `item0${number}.jpg`;
+
+const generateOffers = (count) => (
+  Array(count).fill({}).map(() => ({
+    type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
+    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    description: shuffleArray(DESCRIPTIONS).slice(1, MAX_DESCRIPTION_LENGTH).join(` `),
+    sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
+    picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
+    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+  }))
+);
+
+module.exports = {
+  name: `--generate`,
+  run(args) {
+    const [count] = args;
+    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    const content = JSON.stringify(generateOffers(countOffer));
+
+    fs.writeFile(FILE_NAME, content, (err) => {
+      if (err) {
+        return console.error(`Can't write data to file...`);
+      }
+
+      return console.log(`Operation success. File created.`);
+    });
+  }
+};
