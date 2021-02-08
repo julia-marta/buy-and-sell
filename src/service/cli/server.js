7d8c3acc391aa6a12b78be098e46d02b4bedf136
julia-marta <marta237@yandex.ru>
2021-02-08 14:14:14 +0300

    Добавляет обработку команды server: создаёт новый сервер, описывает функции обработки запросов клиента и отправки ответа, согласно ТЗ, проверяет работу команды, устанавливает Nodemon для автоматического перезапуска сервера

---
+++ src/service/cli/server.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const chalk = require(`chalk`);
+const http = require(`http`);
+const fs = require(`fs`).promises;
+
+const DEFAULT_PORT = 3000;
+const FILENAME = `mocks.json`;
+
+const {
+  HttpCode
+} = require(`../../const`);
+
+const PortRestrict = {
+  MIN: 0,
+  MAX: 65536,
+};
+
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+      <head>
+        <title>With love from Node</title>
+      </head>
+      <body>${message}</body>
+    </html>`.trim();
+
+  res.statusCode = statusCode;
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+
+  res.end(template);
+};
+
+const onClientConnect = async (req, res) => {
+  const notFoundMessageText = `Not found`;
+
+  switch (req.url) {
+    case `/`:
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      }
+
+      break;
+    default:
+      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      break;
+  }
+};
+
+module.exports = {
+  name: `--server`,
+  run(args) {
+
+    const [customPort] = args;
+
+    const portNumber = Number.parseInt(customPort, 10) || DEFAULT_PORT;
+    const port = portNumber >= PortRestrict.MIN && portNumber < PortRestrict.MAX ? portNumber : DEFAULT_PORT;
+
+    http.createServer(onClientConnect)
+    .listen(port)
+    .on(`listening`, (err) => {
+      if (err) {
+        return console.error(chalk.red(`Ошибка при создании сервера`), err);
+      }
+
+      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    });
+  }
+};
