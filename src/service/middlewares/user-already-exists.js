b1bf7c9b6b370002a0ededcc9f091905124a4ef5
julia-marta <marta237@yandex.ru>
2021-04-13 23:59:44 +0300

    Добавляет обработку асинхронных ошибок в мидлвар для проверки существования юзера, переделывает маршруты на фронте на редирект и получений данных из сессий

--- src/service/middlewares/user-already-exists.js
+++ src/service/middlewares/user-already-exists.js
@@ -5,13 +5,21 @@ const {HttpCode, UserMessage} = require(`../../const`);
 module.exports = (service, logger) => async (req, res, next) => {
   const {email} = req.body;
 
-  const userAlreadyExists = await service.findByEmail(email);
+  try {
+    const userAlreadyExists = await service.findByEmail(email);
 
-  if (userAlreadyExists) {
-    logger.error(`User with this email already registered.`);
+    if (userAlreadyExists) {
+      logger.error(`User with this email already registered.`);
 
-    res.status(HttpCode.BAD_REQUEST)
-      .json({errorMessages: [UserMessage.EMAIL_ALREADY_REGISTERED]});
+      res.status(HttpCode.BAD_REQUEST)
+        .json({errorMessages: [UserMessage.EMAIL_ALREADY_REGISTERED]});
+
+      return;
+    }
+
+  } catch (err) {
+    logger.error(`Can't get user service.`);
+    res.status(HttpCode.BAD_REQUEST).json({errorMessages: [`Something wrong`]});
 
     return;
   }
